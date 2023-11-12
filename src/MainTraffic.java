da1e069a575816ba67b58e562db96120c96ac0ae
Misha <mixxerruss@mail.ru>
2023-11-12 15:21:34 +0400

    Added // Hui

--- src/MainTraffic.java
+++ src/MainTraffic.java
@@ -4,6 +4,7 @@ public class MainTraffic {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         String turnOnTraffocLight = scanner.next();
+        // HUI
         TrafficLight switchButton = new TrafficLight();
         switchButton.lightsSwitch(turnOnTraffocLight);
     }
